053d00d0268a54caeb09e22da4ec22f16b5018b3
geonhee <pollux0718@gmail.com>
2025-10-13 15:31:10 +0900

    feat(schema): add SQLAchemy Base and User model definitions

---
+++ app/db/schema.py
@@ -0,0 +1,16 @@
+from sqlalchemy import String, create_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+
+from app.core.config import settings
+
+engine = create_engine(settings.db_url, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True)
+    password: Mapped[str] = mapped_column(String(50))
